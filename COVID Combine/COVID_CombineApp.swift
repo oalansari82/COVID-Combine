1bb5de31974e75808fced37b4b19a74c7a33c64d
Omar Al-Ansari <oalansari@gmail.com>
2021-07-02 06:52:15 +0300

    Initial Commit

---
+++ COVID Combine/COVID_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  COVID_CombineApp.swift
+//  COVID Combine
+//
+//  Created by Omar Al-Ansari on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct COVID_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
